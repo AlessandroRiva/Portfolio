d8c478270dd3256a80d6fb2a30c0ce3a7d5bf9bb
Alessandro Riva <alessandrovittorioriva@gmail.com>
2019-12-22 18:40:48 -0800

    Update

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-function myMove() {
-    const arrow = document.getElementsByClassName('arrows-scroll');
-    let pos = 0;
-    let id = setInterval(frame, 5);
-    arrow.animate.style.marginTop = '-150px', 800;
-    arrow.animate.style.marginBottom = '150px', 800;
-    setTimeout(arrow, 1600);
-}
+// function myMove() {
+//     const arrow = document.getElementsByClassName('arrows-scroll');
+//     let pos = 0;
+//     let id = setInterval(frame, 5);
+//     arrow.animate.style.marginTop = '-150px', 800;
+//     arrow.animate.style.marginBottom = '150px', 800;
+//     setTimeout(arrow, 1600);
+// }
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
